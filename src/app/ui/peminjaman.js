b92560377da7b6ea68f1e761dcf8050e4adf6cb9
omayib-technomotion <technomotion.jogja@gmail.com>
2017-03-11 14:24:00 +0700

    memperbaiki table riwayat peminjaman

--- src/app/ui/peminjaman.js
+++ src/app/ui/peminjaman.js
@@ -23,12 +23,16 @@ export class Peminjaman extends React.Component{
             <div>
                 <Link  to="/pengajuan" activeClassName="active">Buat pengajuan baru</Link>
                 <BootstrapTable data={TransaksiPeminjaman} striped hover selectRow={selectRowProp}>
-                    <TableHeaderColumn isKey dataField='id' >Id</TableHeaderColumn>
-                    <TableHeaderColumn dataField='pic' >Penanggung Jawab</TableHeaderColumn>
-                    <TableHeaderColumn dataField='acara' >Acara</TableHeaderColumn>
-                    <TableHeaderColumn dataField='inventaris' >Inventaris</TableHeaderColumn>
-                    <TableHeaderColumn dataField='tanggal' >Tanggal</TableHeaderColumn>
-                    <TableHeaderColumn dataField='jam' >Waktu</TableHeaderColumn>
+                    <TableHeaderColumn row='0' isKey dataField='id' rowSpan='2' >Id</TableHeaderColumn>
+                    <TableHeaderColumn row='0' dataField='inventaris' rowSpan='2'>Inventaris</TableHeaderColumn>
+                    <TableHeaderColumn row='0' dataField='pic' rowSpan='2' >P.I.C</TableHeaderColumn>
+                    <TableHeaderColumn row='0' dataField='acara' rowSpan='2' >Acara</TableHeaderColumn>
+                    <TableHeaderColumn row='0' colSpan='2' headerAlign='center'>Tanggal</TableHeaderColumn>
+                    <TableHeaderColumn row='1' dataField='tanggal'  headerAlign='center'>pinjam</TableHeaderColumn>
+                    <TableHeaderColumn row='1' dataField='tanggal' headerAlign='center' >kembali</TableHeaderColumn>
+                    <TableHeaderColumn row='0' colSpan='2' headerAlign='center'>Waktu</TableHeaderColumn>
+                    <TableHeaderColumn row='1' dataField='jam' headerAlign='center' >pinjam</TableHeaderColumn>
+                    <TableHeaderColumn row='1' dataField='jam' headerAlign='center' >kembali</TableHeaderColumn>
                 </BootstrapTable>
             </div>
         );
